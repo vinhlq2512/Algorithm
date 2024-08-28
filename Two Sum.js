783ca88e7b1134e57035160faaaeca818d242af5
Lâm Quang Vinh <vinhlq@MacBook-Pro.local>
2024-08-29 06:20:32 +0700

    twoSum

---
+++ Two Sum.js
@@ -0,0 +1,11 @@
+var twoSum = function (nums, target) {
+  const res = {};
+  for (var i = 0; i < nums.length; i++) {
+    var resAbtract = target - nums[i];
+
+    if (res[resAbtract] != null) {
+      return [res[resAbtract], i];
+    }
+    res[nums[i]] = i;
+  }
+};
