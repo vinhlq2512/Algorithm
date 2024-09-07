3e3ba94182f076e53d7b4bad2ddb2e4b7b64c875
vinhlq <vinhlq2512@gmail.com>
2024-09-07 09:30:57 +0700

    Day 2 Container with most water

---
+++ ContainerWithMostWater.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  var left = 0;
+  var right = height.length - 1;
+  var maxArea = 0;
+  while (left < right) {
+    const heightMin = Math.min(height[left], height[right]);
+    const width = right - left;
+    const area = width * heightMin;
+    maxArea = Math.max(maxArea, area);
+    if (height[left] <= height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxArea;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
