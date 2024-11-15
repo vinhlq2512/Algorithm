cd5116eec0137479c52dfb1f4afbeda26327d6ba
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-11-15 15:32:28 +0700

    Time: 73 ms (5.2%), Space: 61.6 MB (5.41%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let res = nums[0];
+  let total = 0;
+
+  for (let n of nums) {
+    if (total < 0) {
+      total = 0;
+    }
+
+    total += n;
+
+    res = res > total ? res : total;
+  }
+
+  return res;
+};
