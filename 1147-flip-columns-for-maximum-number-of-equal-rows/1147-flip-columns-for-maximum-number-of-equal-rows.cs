16ad4968e986f587549d958c4386e4ab170bb696
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-11-22 23:22:24 +0700

    Time: 94 ms (80%), Space: 128.7 MB (7.69%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cs
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cs
@@ -1,40 +1,29 @@
 public class Solution {
     public int MaxEqualRowsAfterFlips(int[][] matrix) {
-        int rows = matrix.Length;
-        int cols = matrix[0].Length;
-        int res = int.MinValue;
         Dictionary<string, int> dic = new Dictionary<string, int>();
-
-        for (int i = 0; i < rows; i++)
+        foreach (int[] row in matrix)
         {
-            string key1 = "";
-            string flipKey = "";
-            for (int j = 0; j < cols; j++)
+            string key = "";
+            for (int j = 0; j < row.Length; j++)
             {
-                 key1 += matrix[i][j].ToString();
-                 
-                 int keyStr = matrix[i][j] ^ 1;
-                 flipKey += keyStr.ToString();
+                if (row[0] == row[j])
+                {
+                    key += "T";
+                }
+                else
+                {
+                    key += "F";
+                }
             }
-            if (dic.ContainsKey(key1))
+            if (dic.ContainsKey(key))
             {
-                dic[key1]++;
-            }
-            else
-            {
-                dic.Add(key1,1);
-            }
-            
-            if (dic.ContainsKey(flipKey))
-            {
-                dic[flipKey]++;
+                dic[key]++;
             }
             else
             {
-                dic.Add(flipKey,1);
+                dic.Add(key,1);
             }
         }
-        
         return dic.Values.Max();
     }
 
