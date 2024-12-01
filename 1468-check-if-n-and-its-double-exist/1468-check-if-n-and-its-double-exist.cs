924c2d8f652921702f283573c876dc4cd373275b
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-12-01 17:01:10 +0700

    Time: 2 ms (29.84%), Space: 44.2 MB (6.69%) - LeetHub

---
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool CheckIfExist(int[] arr) {
+        int n = arr.Length;
+        HashSet<int> check = new HashSet<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (check.Contains(arr[i] * 2) || 
+                (arr[i] % 2 == 0 && check.Contains(arr[i] / 2)))
+            {
+                return true;
+            }
+            check.Add(arr[i]);
+        }
+        return false;
+    }
+}
