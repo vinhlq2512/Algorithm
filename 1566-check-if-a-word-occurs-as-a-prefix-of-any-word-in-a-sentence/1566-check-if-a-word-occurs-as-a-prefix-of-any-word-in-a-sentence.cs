f6377da925d4d7bfde539e47d334c94ee4715ada
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-12-02 09:32:09 +0700

    Time: 0 ms (100%), Space: 38 MB (46.34%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cs
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cs
@@ -1,15 +1,23 @@
 public class Solution {
     public int IsPrefixOfWord(string sentence, string searchWord)
     {
-
+        int n = searchWord.Length;
         string[] splited = sentence.Split(' ');
 
         for (int i = 0; i < splited.Length; i++)
         {
-            if (splited[i].StartsWith(searchWord))
+            int j = 0;
+            while (j < n && splited[i].Length >= n)
             {
-                return i + 1; 
+                if (searchWord[j] != splited[i][j])
+                    break;
+                j++;
+                if (j == n)
+                {
+                    return i + 1;
+                }
             }
+
         }
 
         return -1;
