8ffdf01371a4c7676e3f329a22bad2bb562eb226
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-12-03 23:51:27 +0700

    Time: 7 ms (90%), Space: 84.8 MB (64%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cs
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cs
@@ -2,11 +2,12 @@ public class Solution {
     public string AddSpaces(string s, int[] spaces) {
         StringBuilder res = new StringBuilder();
         int n = s.Length;
+        res.EnsureCapacity(n + spaces.Length);
         int i = 0;
         int j = 0;
         while (i < n)
         {
-            if ( j < spaces.Length && i == spaces[j])
+            if (j < spaces.Length && i == spaces[j])
             {
                 res.Append(" " );
                 j++;
