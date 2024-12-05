6964be9bc24873036b41af2d1f7b19f79ba94747
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-12-06 06:44:50 +0700

    Time: 2 ms (58.21%), Space: 43 MB (29.81%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -1,15 +1,25 @@
 public class Solution {
     public string LongestCommonPrefix(string[] strs)
     {
-        StringBuilder sb = new StringBuilder();
+        int minLength = int.MaxValue;
+        string strCompare = "";
 
-        string strCompare = strs[0];
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (strs[i].Length < minLength)
+            {
+                minLength = strs[i].Length;
+                strCompare = strs[i];
+            }
+        }
+        StringBuilder sb = new StringBuilder(minLength);
+
         for (int i = 0; i < strCompare.Length; i++)
         {
             sb.Append(strCompare[i]);
-            for (int j = 1; j < strs.Length; j++)
+            for (int j = 0; j < strs.Length; j++)
             {
-                if (!strs[j].StartsWith(sb.ToString()))
+                if (!strs[j][i].Equals(sb[i]))
                 {
                     return sb.Remove(sb.Length - 1, 1).ToString();
                 }
