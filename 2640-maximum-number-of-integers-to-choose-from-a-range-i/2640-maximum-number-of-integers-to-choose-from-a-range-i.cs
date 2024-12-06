19e4c0359eb2a128d5fffb88bc25a9deb0eec33b
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-12-06 09:04:54 +0700

    Time: 242 ms (30.77%), Space: 61.3 MB (100%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cs
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cs
@@ -1,12 +1,13 @@
 public class Solution {
     public int MaxCount(int[] banned, int n, int maxSum)
     {
+        Array.Sort(banned);
         int start = 1;
         int count = 0;
 
         while (start <= n)
         {
-            if (!isBanned(banned, start))
+            if (!IsBanned(banned, start))
             {
                 maxSum = maxSum - start;
                 if (maxSum < 0)
@@ -21,15 +22,28 @@ public class Solution {
         }
         return count;
     }
-    public bool isBanned(int[] banned, int number)
+
+
+    public bool IsBanned(int[] banned, int number)
     {
-        for (int i = 0; i < banned.Length; i++)
+        int left = 0, right = banned.Length - 1;
+        while (left <= right)
         {
-            if (banned[i] == number)
+            int mid = left + (right - left) / 2;
+            if (banned[mid] == number)
             {
                 return true;
             }
+            else if (banned[mid] < number)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
         }
+
         return false;
     }
 }
