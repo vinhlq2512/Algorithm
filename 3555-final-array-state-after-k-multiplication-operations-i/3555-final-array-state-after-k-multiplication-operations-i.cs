0042cfb8669ff3f9626453c3433224063e38cc3f
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-12-16 08:51:11 +0700

    Time: 1 ms (100%), Space: 49.4 MB (66.22%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cs
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cs
@@ -1,18 +1,20 @@
 public class Solution {
     public int[] GetFinalState(int[] nums, int k, int multiplier)
     {
-        var maxHeap = new PriorityQueue<(int value, int index), (int priority, int index)>();
-        for (int i = 0; i < nums.Length; i++)
-        {
-            maxHeap.Enqueue((nums[i], i), (nums[i], i));
-        }
+    
         for (int i = 0; i < k; i++)
         {
-            var (value, index) = maxHeap.Dequeue();
-            int newValue = value * multiplier;
-
-            nums[index] = newValue;
-            maxHeap.Enqueue((newValue, index), (newValue, index));
+            int min = int.MaxValue;
+            int idx = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] < min)
+                {
+                    min = nums[j];
+                    idx = j;
+                }
+            }
+            nums[idx] = min * multiplier;
         }
 
         return nums;
