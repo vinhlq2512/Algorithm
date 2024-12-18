b641b386e3908f6e8f6306ffeff2e67db7792c2a
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-12-18 13:33:04 +0700

    Time: 1 ms (96.25%), Space: 47.1 MB (22.5%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] FinalPrices(int[] prices)
+    {
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            for (int j = 1; j < prices.Length; j++)
+            {
+                if (j > i && prices[j] <= prices[i])
+                {
+                    prices[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+}
