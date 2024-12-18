542b19bda51b7f5a26c89f2fde0715125ee0728b
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2024-12-18 13:52:08 +0700

    Time: 3 ms (22.5%), Space: 47.5 MB (7.5%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cs
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cs
@@ -1,17 +1,27 @@
 public class Solution {
     public int[] FinalPrices(int[] prices)
     {
-        for (int i = 0; i < prices.Length - 1; i++)
+        int n = prices.Length;
+        int[] res = (int[])prices.Clone();
+
+        Stack<(int, int)> stack = new Stack<(int, int)>();
+        for (int i = n - 1; i >= 0; i--)
         {
-            for (int j = i + 1; j < prices.Length; j++)
+            stack.Push((prices[i], i));
+        }
+        while (stack.Count > 0)
+        {
+            (int value, int index) = stack.Pop();
+            for (int j = index + 1; j < n; j++)
             {
-                if (prices[j] <= prices[i])
+                if (value >= prices[j])
                 {
-                    prices[i] = prices[i] - prices[j];
+                    res[index] = value - prices[j];
                     break;
                 }
             }
+
         }
-        return prices;
+        return res;
     }
 }
