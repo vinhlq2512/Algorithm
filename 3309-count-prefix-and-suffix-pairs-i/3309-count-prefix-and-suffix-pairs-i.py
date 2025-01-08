6ae8a5d1f6154c3f5a9ecf7cf77ba1b547eac20f
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2025-01-08 23:52:29 +0700

    Time: 23 ms (7.24%), Space: 17.6 MB (34.51%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.py
@@ -3,7 +3,15 @@ class Solution:
         res = 0
 
         def isPrefixAndSuffix(str1: str, str2: str) -> bool:
-            return str2.startswith(str1) and str2.endswith(str1)
+            if len(str1) > len(str2):
+                return False
+            
+            n = len(str1) 
+
+            for i in range(n):
+                if str1[len(str1) - i - 1] != str2[len(str2) - 1 - i] or str1[i] != str2[i]:
+                    return False
+            return True
 
         for i in range(len(words)):
             for j in range(i + 1, len(words)):
