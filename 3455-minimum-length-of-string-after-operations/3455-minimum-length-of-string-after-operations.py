94a73c7f47bff697259dce3d8ed726b2bf11ca13
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2025-01-14 00:14:58 +0700

    Time: 306 ms (14.28%), Space: 18.9 MB (14.92%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -1,17 +1,15 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        char_frequency = [0] * 26
         total_length = 0
+        freq_char = [0] * 26;
+        for i in range(len(s)):
+            freq_char[ord(s[i]) - ord('a')] += 1;
 
-        for current_char in s:
-            char_frequency[ord(current_char) - ord("a")] += 1
-
-        for frequency in char_frequency:
-            if frequency == 0:
-                continue
-            if frequency % 2 == 0:
-                total_length += 2
-            else:
-                total_length += 1
+        for i in range(26):
+           if freq_char[i] > 0:
+               if freq_char[i] % 2 == 1:
+                   total_length += 1
+               else:
+                   total_length += 2
 
         return total_length
