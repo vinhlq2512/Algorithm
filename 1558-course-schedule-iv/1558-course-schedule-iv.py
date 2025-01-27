ac6c8b6c798efc2edd7dd3627b2fd6b72f478523
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2025-01-27 20:58:04 +0700

    Time: 633 ms (21.51%), Space: 20.4 MB (61.44%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isPrerequisite(
+        self, adjList: dict, visited: List[bool], src: int, target: int
+    ) -> bool:
+        visited[src] = True
+
+        if src == target:
+            return True
+
+        for adj in adjList.get(src, []):
+            if not visited[adj]:
+                if self.isPrerequisite(adjList, visited, adj, target):
+                    return True
+        return False
+
+    def checkIfPrerequisite(
+        self,
+        numCourses: int,
+        prerequisites: List[List[int]],
+        queries: List[List[int]],
+    ) -> List[bool]:
+        adjList = {i: [] for i in range(numCourses)}
+
+        for edge in prerequisites:
+            adjList[edge[0]].append(edge[1])
+
+        result = []
+
+        for query in queries:
+            visited = [False] * numCourses
+            result.append(
+                self.isPrerequisite(adjList, visited, query[0], query[1])
+            )
+
+        return result
