19c124c4a45e1671c4913cc1124c41bec0e20f2d
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2025-02-07 22:55:03 +0700

    Time: 61 ms (90.63%), Space: 63.2 MB (58.08%) - LeetHub

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.py
@@ -0,0 +1,23 @@
+class Solution:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        n = len(queries)
+        result = []
+        color_map = {}
+        ball_map = {}
+
+        for i in range(n):
+            ball, color = queries[i]
+
+            if ball in ball_map:
+                prev_color = ball_map[ball]
+                color_map[prev_color] -= 1
+
+                if color_map[prev_color] == 0:
+                    del color_map[prev_color]
+
+            ball_map[ball] = color
+            color_map[color] = color_map.get(color, 0) + 1
+
+            result.append(len(color_map))
+
+        return result
