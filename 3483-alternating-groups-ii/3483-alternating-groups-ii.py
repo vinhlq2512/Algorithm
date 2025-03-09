4cb77060afd0426f4ae1005d97d3e634f9f35e37
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2025-03-09 22:49:06 +0700

    Time: 817 ms (12.96%), Space: 21.7 MB (34.88%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        colors = colors + colors[:k - 1]  # Extend for circular behavior
+        res = 0
+        left = 0
+
+        for right in range(1, len(colors)):
+            if colors[right] == colors[right - 1]:
+                left = right
+
+            if right - left + 1 == k:
+                res += 1
+                left += 1  
+
+        return res
