eb6b848600d1a72ec48072e4baa5bf6bcf425735
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2025-03-15 16:33:19 +0700

    Time: 666 ms (9.25%), Space: 29.9 MB (18.93%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -4,16 +4,14 @@ class Solution:
             return 0  
 
         left, right = 1, max(candies)
-        res = 0
         
-        while left <= right:
-            mid = (left + right + 1) // 2
-            count = sum(c // mid for c in candies)  
-            
-            if count >= k:  
-                res = mid  
-                left = mid + 1  
+        while left < right:
+            mid = (left + right + 1) // 2  
+            count = sum((c - (c % mid)) // mid for c in candies) 
+
+            if count >= k:
+                left = mid  
             else:
                 right = mid - 1  
         
-        return res
+        return left
